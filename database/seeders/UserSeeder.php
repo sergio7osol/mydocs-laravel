69d02e4d6a5d49ca7aefa0977c5a45bd047a9855
Sergey Osokin <sergey.osokin@netfire.eu>
2025-06-25 21:56:55 +0200

    feat: add database seeders for users and documents.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin user
+        User::factory()->create([
+            'first_name' => 'Admin',
+            'last_name' => 'User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => true,
+        ]);
+
+        // Create regular users
+        User::factory(3)->create();
+    }
+}
